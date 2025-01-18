84b900ac05464795fc9f50e8f31a1d1c810b4c64
Omergottlieb33 <omer.gottlieb@gmail.com>
2025-01-18 14:20:15 +0200

    utils

--- src/utils/plot_utils.py
+++ src/utils/plot_utils.py
@@ -1,8 +1,9 @@
+import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from utils.common_utils import xywh_to_x1y1x2y2, get_location_factor
-from config.const import IMAGE_SIZE
+from src.utils.common_utils import xywh_to_x1y1x2y2, get_location_factor
+from src.config.const import IMAGE_SIZE
 
 
 def show_frame_with_annotations(frame, box_list, title):
@@ -26,3 +27,8 @@ def get_laser_intesity_facotr():
         for y in range(0,IMAGE_SIZE[1]-1):
             intensity_map[x,y] = get_location_factor(x,y)
     return intensity_map
+
+def draw_yolo_results_on_frame(results, output_dir):
+    for i, result in enumerate(results):
+        img_path = os.path.join(output_dir, f'frame_{i:04d}.png')
+        result.save(img_path)
